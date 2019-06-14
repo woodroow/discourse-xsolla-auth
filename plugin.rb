688bc4b574fba80aff7688c8fbef7b4741fac36c
Artem Veshkurov <a.veshkurov@xsolla.com>
2019-06-15 04:56:15 +0500

    changed

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,7 @@ enabled_site_setting :xsolla_auth_secret
 
 require_dependency 'auth/oauth2_authenticator'
 
-gem "discourse-omniauth-jwt", :git => "git://github.com/woodroow/discourse-omniauth-jwt.git"
+gem "discourse-omniauth-jwt", "0.0.2", git: 'https://github.com/woodroow/discourse-omniauth-jwt', require: false
 
 require 'omniauth/jwt'
 
