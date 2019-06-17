1045b39fe339dfb74a9a48e6db09e1c179a867d5
Artem Veshkurov <a.veshkurov@xsolla.com>
2019-06-18 00:20:59 +0500

    1

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,7 @@ enabled_site_setting :xsolla_auth_secret
 
 require_dependency 'auth/oauth2_authenticator'
 
-gem "discourse-omniauth-jwt-xsolla", "0.0.5", require: false
+gem "discourse-omniauth-jwt-xsolla", "0.0.6", require: false
 
 require 'omniauth/jwt'
 
